efa4d150d8c45de2bb02c6b620ad0549a3571682
Dmitry Parkhomenko <oku_74@mail.ru>
2024-02-29 15:06:57 +0500

    Initial Commit

---
+++ TrafficLight/TrafficLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightApp.swift
+//  TrafficLight
+//
+//  Created by Dmitry Parhomenko on 29.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
