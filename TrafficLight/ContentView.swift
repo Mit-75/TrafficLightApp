3cd26f1327fb1a6c1b7458e5da2f728313e3ed18
Dmitry Parkhomenko <oku_74@mail.ru>
2024-03-01 11:11:58 +0500

    - Mission completed

--- TrafficLight/ContentView.swift
+++ TrafficLight/ContentView.swift
@@ -8,14 +8,86 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var text = "START"
+
+    @State private var redAlpha = 0.3
+    @State private var yellowAlpha = 0.3
+    @State private var greenAlpha = 0.3
+    
+    @State private var currentLight = CurrentLight.red
+     private let lightIsOn = 1.0
+     private let lightIsOff = 0.3
+    
+    
     var body: some View {
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Circle()
+                .foregroundStyle(Color.red)
+                .opacity(redAlpha)
+                .frame(width: 100, height: 100)
+                .overlay(Circle().stroke(Color.white, lineWidth: 4))
+                .shadow(color: Color.red, radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/ )
+            
+            Circle()
+                .foregroundStyle(Color.yellow)
+                .opacity(yellowAlpha)
+                .frame(width: 100, height: 100)
+                .overlay(Circle().stroke(Color.white, lineWidth: 4))
+                .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+                .shadow(color: Color.yellow, radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/ )
+                .padding(15)
+            
+            Circle()
+                .foregroundStyle(Color.green)
+                .opacity(greenAlpha)
+                .frame(width: 100, height: 100)
+                .overlay(Circle().stroke(Color.white, lineWidth: 4))
+                .shadow(color: Color.green, radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/ )
+            
+            Spacer()
+            
+            Button {
+                tapButton()
+            } label: {
+                Text("\(text)")
+                    .font(.system(.title, design: .rounded, weight: .heavy))
+                    .foregroundColor(.black)
+            }
+            .padding(EdgeInsets(top: 15, leading: 30, bottom: 15, trailing: 30))
+            .background(
+                LinearGradient(gradient: Gradient(colors: [.red, .green]), startPoint: .topLeading, endPoint: .bottomTrailing)
+            )
+            .cornerRadius(20.0)
         }
-        .padding()
+        .padding(50)
+    }
+    
+    private func tapButton() {
+        text = ("NEXT")
+        
+        switch currentLight {
+        case .red:
+            greenAlpha = lightIsOff
+            redAlpha = lightIsOn
+            currentLight = .yellow
+        case .yellow:
+            redAlpha = lightIsOff
+            yellowAlpha = lightIsOn
+            currentLight = .green
+        case .green:
+            greenAlpha = lightIsOn
+            yellowAlpha = lightIsOff
+            currentLight = .red
+        }
+    }
+}
+
+// MARK: - CurrentLight
+extension ContentView {
+    private enum CurrentLight {
+        case red, yellow, green
     }
 }
 
--- TrafficLight/TrafficLightApp.swift
+++ TrafficLight/TrafficLightApp.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 @main
 struct TrafficLightApp: App {
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
